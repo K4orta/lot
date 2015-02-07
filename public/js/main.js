91476d56c5307ccb6f6e07c4cf990036faf86f9a
ewong <ewong@blizzard.com>
2015-02-07 00:16:13 -0800

    Basic sprite code

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
 
-// console.log('Hello World!!');
-
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 	preload: preload,
 	create: create,
@@ -8,13 +6,31 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 function preload() {
-
+	game.load.image('star', 'assets/star.png');
 }
 
+var star;
+var listener = function(e) {
+
+};
+
 function create() {
+	star = game.add.sprite(0, 0, 'star');
+	game.physics.arcade.enable(star);
 	
+	star.body.bounce.y = 0.3;
+	star.body.collideWorldBounds = true;
+
+	star.inputEnabled = true;
+    star.input.enableDrag(true);
+    star.events.onInputDown.add(listener, this);
 }
 
 function update() {
-	
+	if (game.input.mousePointer.isDown) {
+		star.body.velocity.y = 0;
+		star.body.gravity.y = 0;
+	} else {
+		star.body.gravity.y = 300;
+	}
 }
